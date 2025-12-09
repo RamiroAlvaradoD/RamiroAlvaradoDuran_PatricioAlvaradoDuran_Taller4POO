e008ba30659ff2b6f74dc319d17d959accff624a
RamiroAlvaradoD <ramiro.alvarado@alumnos.ucn.cl>
2025-12-09 01:13:01 -0300

    Directorio principal arreglado
    Se arregló la estructura del directorio, las clases iniciales fueron agregadas otra vez

---
+++ src/dominio/Administrador.java
@@ -0,0 +1,9 @@
+package dominio;
+
+public class Administrador extends Usuario {
+
+	public Administrador(String username, String password, String rol) {
+		super(username, password, rol);
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ src/dominio/Estudiante.java
@@ -0,0 +1,42 @@
+package dominio;
+
+public class Estudiante extends Usuario {
+
+	private String rut;
+	private String nombre;
+	private String carrera;
+	private int semestre;
+	private String correo;
+
+	public Estudiante(String rut, String nombre, String carrera, int semestre, String correo, String password) {
+
+		super(correo, password, "Estudiante");
+
+		this.rut = rut;
+		this.nombre = nombre;
+		this.carrera = carrera;
+		this.semestre = semestre;
+		this.correo = correo;
+	}
+
+	public String getRut() {
+		return rut;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getCarrera() {
+		return carrera;
+	}
+
+	public int getSemestre() {
+		return semestre;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+}
